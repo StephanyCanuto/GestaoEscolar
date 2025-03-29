939eb1667043e02f596498b9777f930631a3b240
StephanyCanuto <stephany.canutos25@gmail.com>
2025-03-29 12:00:03 -0300

    feat: adicionando classe professor

---
+++ src/models/Professor.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Professor {
+    private String nomeProfessor;
+    private String disciplinaProfessor;
+    private int idadeProfessor;
+
+    public Professor(String nomeProfessor, String disciplinaProfessor, int idadeProfessor) {
+        this.nomeProfessor = nomeProfessor;
+        this.disciplinaProfessor = disciplinaProfessor;
+        this.idadeProfessor = idadeProfessor;
+    }
+
+    public String getNomeProfessor() {
+        return nomeProfessor;
+    }
+
+    public String getDisciplinaProfessor() {
+        return disciplinaProfessor;
+    }
+
+    public int getIdadeProfessor() {
+        return idadeProfessor;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nomeProfessor" + nomeProfessor + "/" +
+                "idadeProfessor" + idadeProfessor + "/"+
+                "disciplinaProfessor" + disciplinaProfessor +
+        '}';
+    }
+}
